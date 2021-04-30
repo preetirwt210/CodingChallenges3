850b78225aaa875eefeb433f3d94c7c9c898e50b
Preeti Rawat <preetirawat711@gmail.com>
2021-04-30 18:32:25 +0530

    Question1

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        SortedArray sa=new SortedArray();
+        int[] myIntegers=SortedArray.getIntegers(5);
+        int[] sortedArray= SortedArray.sortIntegers(myIntegers);
+       SortedArray.printArray(sortedArray);
     }
 }
