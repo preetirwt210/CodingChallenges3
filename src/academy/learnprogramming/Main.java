7962179567acf475dab94973dc7f5d006ea2d353
Preeti Rawat <preetirawat711@gmail.com>
2021-05-04 17:50:50 +0530

    Question2

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -4,9 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        SortedArray sa=new SortedArray();
+      /*  SortedArray sa=new SortedArray();
         int[] myIntegers=SortedArray.getIntegers(5);
         int[] sortedArray= SortedArray.sortIntegers(myIntegers);
-       SortedArray.printArray(sortedArray);
+       SortedArray.printArray(sortedArray); */
+
+      int wow=MinimumElement.readInteger();
+      int[] ok=MinimumElement.readElements(wow);
+      System.out.println("Minimum value in the array is: "+ MinimumElement.findMin(ok) );
     }
 }
--- src/academy/learnprogramming/MinimumElement.java
+++ src/academy/learnprogramming/MinimumElement.java
@@ -1,5 +1,34 @@
 package academy.learnprogramming;
 
+import java.util.Scanner;
+
 public class MinimumElement {
-    minimum
+
+    public static int readInteger(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enters count: ");
+        int readInteger=scanner.nextInt();
+        return readInteger;
+    }
+    public static int[] readElements(int number){
+        Scanner scanner=new Scanner(System.in);
+         int[] array=new int[number];
+         for(int i=0;i<array.length;i++){
+             System.out.println("Enter values: ");
+             int totalElementEntered=scanner.nextInt();
+             scanner.nextLine();
+             array[i]=totalElementEntered;
+
+         }
+        scanner.close();
+        return array ;
+    }
+    public static int findMin(int[] minValue){ //{3,4,2,5,1}
+      int min=Integer.MAX_VALUE;// min= max value;
+      for(int i=0;i<minValue.length;i++){//0-4
+          if (minValue[i]<min) {//3<min true;//4<3 false;//2<3 true//5<2 false//1<2 true
+              min=minValue[i];//min=1;
+          }
+      }return min;
+    }
 }
