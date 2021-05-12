14239e50a5d7d37a8d74adc76077e527b4aa5821
Preeti Rawat <preetirawat711@gmail.com>
2021-05-12 17:45:08 +0530

    Question10 Example of Abstract Class

---
+++ src/academy/learnprogramming/NodeList.java
@@ -0,0 +1,8 @@
+package academy.learnprogramming;
+
+public interface NodeList {
+    abstract ListItem getRoot();
+    abstract boolean addItem(ListItem newItem);
+    abstract boolean removeItem(ListItem item);
+    abstract void traverse(ListItem root);
+}
